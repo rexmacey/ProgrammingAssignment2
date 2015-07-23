8652129298ab8c006c05fa5af348a59fa1ef1c10
Rex Macey <rex@macey.us>
2015-07-22 22:48:27 -0400

    Show message
    Show caching message

--- cachematrix.R
+++ cachematrix.R
@@ -23,7 +23,7 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     m<-x$getinverse()
     if(!is.null(m)){
-        #message("getting cached data")
+        message("getting cached data")
         return(m)
     }
     data<-x$get()
